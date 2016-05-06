71fef9436e823a9a7947dacdaa6b9b889477da93
Erik Newton <erik_newton@hotmail.com>
2016-05-06 22:36:09 +0100

    More tests

--- test/test_scrit.rb
+++ test/test_scrit.rb
@@ -17,4 +17,26 @@ class ScritTest < Minitest::Test
   def test_default_direction
     assert_equal 0, @sprite.direction
   end
+  
+  def test_set_x
+    @sprite.set_x_to(5)
+    assert_equal 5, @sprite.x_position
+  end
+  
+  def test_change_x
+    @sprite.set_x_to(-2)
+    @sprite.change_x_by(6)
+    assert_equal 4, @sprite.x_position
+  end
+  
+  def test_set_y
+    @sprite.set_y_to(5)
+    assert_equal 5, @sprite.y_position
+  end
+  
+  def test_change_y
+    @sprite.set_y_to(-2)
+    @sprite.change_y_by(6)
+    assert_equal 4, @sprite.y_position
+  end
 end
